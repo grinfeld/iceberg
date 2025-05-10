1d15aec0343a6b4bca3a287635781ecd973eac69
michael.grinfeld <michael.grinfeld@dynamicyield.com>
2025-05-10 11:02:43 +0300

    refactor configuration

--- src/main/scala/com/example/AvroToIcebergExample.scala
+++ src/main/scala/com/example/AvroToIcebergExample.scala
@@ -1,75 +1,32 @@
 package com.example
 
-import com.typesafe.config.Config
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.avro.functions.from_avro
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.avro.SchemaConverters
-import com.dy.rawV2.gen.RawEventV2
+import com.example.SparkFlowIceberg.{ConfigParser, ConfigStarter}
 
 object AvroToIcebergExample {
   def main(args: Array[String]): Unit = {
     val profile = System.getProperty("PROFILE")
 
-    val parser = ConfigParser(profile)
+    val sparkFlow =  profile
+      .createConfig()
+      .sparkFlow()
 
-    // Load configuration
-    val config = parser.config
-    val appConfig: Config = config.getConfig("app")
-
-    // Create Spark session with configurations
-    var sparkBuilder: SparkSession.Builder = SparkSession.builder()
-      .appName(appConfig.getString("name"))
-
-    if(appConfig.hasPath("master"))
-      sparkBuilder = sparkBuilder.master(appConfig.getString("master"))
-
-    sparkBuilder = parser.applyConfig("app.config", sparkBuilder)
-
-    val spark = sparkBuilder.getOrCreate()
-
-    val avroData = spark.read.format("avro").load(appConfig.getString("s3.from"))
-
-    // Convert to RawEventV2 format
-    val rawEvents = avroData.select(
-      from_avro(col("value"), RawEventV2.getClassSchema.toString).as("raw_event")
-    ).select("raw_event.*")
-
-    val iceberg = parser.icebergConf()
-
-    if (config.getBoolean("app.create.table")) {
-      createTable(spark, iceberg)
-    }
+    val icebergConf = sparkFlow.icebergConf()
 
     // Write to Iceberg table - Iceberg will automatically handle schema evolution
-    rawEvents.writeTo(s"${iceberg.database}.${iceberg.table}").append()
+    val rawEvents = sparkFlow.readParquet()
+
+    // Write to Iceberg table - Iceberg will automatically handle schema evolution
+    rawEvents.writeTo(icebergConf.fullTableName).append()
 
     // Print current schema and data
     println("\nCurrent table schema:")
-    spark.table(s"${iceberg.database}.${iceberg.table}").printSchema()
+    sparkFlow.spark.table(icebergConf.fullTableName).printSchema()
     
     println("\nData in Iceberg table:")
-    spark.table(s"${iceberg.database}.${iceberg.table}").show(5, truncate = false)
+    sparkFlow.spark.table(icebergConf.fullTableName).show(5, truncate = false)
 
-    spark.stop()
+    sparkFlow.spark.stop()
   }
 
-  private def createTable(spark: SparkSession, iceberg: IcebergConf): Unit = {
-    // Get Avro schema and convert to Spark SQL schema using built-in converter
-    // Check if table exists
-    val tableExists = spark.catalog.tableExists(iceberg.database, iceberg.table)
-    if (tableExists) {
-      val avroSchema = RawEventV2.getClassSchema
-      val sparkSchema = SchemaConverters.toSqlType(avroSchema).dataType.asInstanceOf[org.apache.spark.sql.types.StructType]
-      // Create table with initial schema
-      println("Creating new table with schema:")
 
-      spark.sql(
-        s"""
-        CREATE TABLE ${iceberg.fullTableName} (
-          ${sparkSchema.fields.map(field => s"${field.name} ${field.dataType.sql}").mkString(",\n\t\t")}
-        ) USING iceberg
-      """)
-    }
-  }
 } 
---
+++ src/main/scala/com/example/SparkFlowIceberg.scala
@@ -0,0 +1,124 @@
+package com.example
+
+import com.dy.rawV2.gen.RawEventV2
+import com.typesafe.config.{Config, ConfigFactory, ConfigValue, ConfigValueType}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.avro.SchemaConverters
+
+import scala.collection.JavaConverters.iterableAsScalaIterableConverter
+
+object SparkFlowIceberg {
+
+  case class SparkFlowIceberg(spark: SparkSession, config: Config) {
+
+      def readParquet(): DataFrame = {
+        val df = spark.read.parquet(config.getString("app.s3.from"))
+        createTableIfNotExist()
+        df
+      }
+
+      def stop(): Unit = {
+        spark.stop()
+      }
+
+      def table(name: String): DataFrame = {
+        spark.table(name)
+      }
+
+      def icebergConf(): IcebergConf = {
+        config.icebergConf()
+      }
+
+      private def createTableIfNotExist(): Unit = {
+        val iceberg: IcebergConf = config.icebergConf()
+        // Get Avro schema and convert to Spark SQL schema using built-in converter
+        // Check if table exists
+        val tableExists = spark.catalog.tableExists(iceberg.database, iceberg.table)
+        if (tableExists) {
+          val avroSchema = RawEventV2.getClassSchema
+          val sparkSchema = SchemaConverters.toSqlType(avroSchema).dataType.asInstanceOf[org.apache.spark.sql.types.StructType]
+          // Create table with initial schema
+          println("Creating new table with schema:")
+
+          spark.sql(
+            s"""
+              CREATE TABLE ${iceberg.fullTableName} (
+                ${sparkSchema.fields.map(field => s"${field.name} ${field.dataType.sql}").mkString(",\n\t\t")}
+              ) USING iceberg
+            """)
+        }
+      }
+
+  }
+
+  implicit class ConfigParser (config: Config) {
+
+    def sparkFlow(): SparkFlowIceberg = {
+      val appConfig: Config = config.getConfig("app")
+      var sparkBuilder = SparkSession.builder()
+        .appName(appConfig.getString("name"))
+      if(appConfig.hasPath("master"))
+        sparkBuilder = sparkBuilder.master(appConfig.getString("master"))
+
+      applyConfig(config.getConfig("app.config"), "", (key, value) => {
+        value match {
+          case d: Double => sparkBuilder.config(key, d)
+          case l: Long => sparkBuilder.config(key, l)
+          case b: Boolean => sparkBuilder.config(key, b)
+          case any: Any => sparkBuilder.config(key, any.toString)
+        }
+      })
+
+      SparkFlowIceberg(sparkBuilder.getOrCreate(), config)
+    }
+
+    def icebergConf(): IcebergConf = {
+      IcebergConf(config.getString("app.iceberg.database"), config.getString("app.iceberg.table"))
+    }
+
+    private def applyConfig(config: Config, prefix: String = "", setFn: (String, Any) => Unit): Unit = {
+      config.entrySet().forEach { entry =>
+        val key = entry.getKey
+        val value: ConfigValue = entry.getValue
+
+        val nextPrefix = if ("".equals(prefix)) key else s"$prefix.$key"
+        if ("def-impl".equals(key)) {
+          // spark.conf.set("spark.sql.catalog.<catalog name>", "org.apache.iceberg.spark.SparkCatalog")
+          // or custom one
+          setFn(nextPrefix, value.unwrapped().toString)
+        } else {
+          value.valueType() match {
+            case ConfigValueType.LIST =>
+              if ("".equals(nextPrefix))
+                throw new IllegalArgumentException("in spark: list can not start with empty config")
+              val list = config.getList(key).unwrapped().asScala.zipWithIndex
+              // we assume all of them string
+              for ((value, ind) <- list) {
+                setFn(s"$nextPrefix[$ind]", value.toString)
+              }
+              val res = config.getList(key).unwrapped().asScala.map(_.toString).mkString(",")
+              setFn(nextPrefix, res)
+            case ConfigValueType.NULL => return
+            case ConfigValueType.STRING =>
+              setFn(nextPrefix, config.getString(key))
+            case ConfigValueType.BOOLEAN =>
+              setFn(nextPrefix, config.getBoolean(key))
+            case ConfigValueType.NUMBER =>
+              if (value.unwrapped().toString.contains("."))
+                setFn(nextPrefix, value.unwrapped().asInstanceOf[Number].doubleValue())
+              else
+                setFn(nextPrefix, value.unwrapped().asInstanceOf[Number].longValue())
+            case ConfigValueType.OBJECT =>
+              applyConfig(config.getConfig(nextPrefix), nextPrefix, setFn)
+          }
+        }
+      }
+    }
+  }
+
+  implicit class ConfigStarter(profile: String) {
+    def createConfig(): Config = {
+      ConfigFactory.load(s"application.$profile.conf")
+    }
+  }
+}
