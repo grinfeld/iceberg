5e80cffb33a259c8010acf90acd7a11ef712ea11
grinfeld <grinfeld@gmail.com>
2025-05-10 14:54:26 +0300

    almost works

--- src/main/scala/com/example/IcebergApp.scala
+++ src/main/scala/com/example/IcebergApp.scala
@@ -2,7 +2,15 @@ package com.example
 
 object IcebergApp {
   def main(args: Array[String]): Unit = {
-    val profile = System.getProperty("PROFILE")
+    val profile = findProfile()
     SimpleIcebergApp.runApp(profile)
+  }
+
+  private def findProfile(): String = {
+    var profile = System.getenv("PROFILE")
+    if (profile == null || "".equals(profile))
+      profile = System.getProperty("PROFILE")
+
+    profile
   }
 }
